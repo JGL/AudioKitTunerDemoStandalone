e5ae1b0909d1501b9e13f1781caaa33f0ac86701
Joel Gethin Lewis <me@joelgethinlewis.com>
2025-05-07 17:29:36 +0100

    Pushing demonstration app for the first time

---
+++ AudioKitTunerDemoStandalone/AudioKitTunerDemoStandaloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKitTunerDemoStandaloneApp.swift
+//  AudioKitTunerDemoStandalone
+//
+//  Created by Joel Lewis on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioKitTunerDemoStandaloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
