a0f1e645db7281b81951c194ac585ccedd7a2a1d
Joel Gethin Lewis <me@joelgethinlewis.com>
2025-05-14 16:37:04 +0100

    Fixing resume bug thanks to NickCulbertson suggestion

--- AudioKitTunerDemoStandalone/ContentView.swift
+++ AudioKitTunerDemoStandalone/ContentView.swift
@@ -23,36 +23,36 @@ struct TunerData {
 
 class TunerConductor: ObservableObject, HasAudioEngine {
     @Published var data = TunerData()
-
+    
     let engine = AudioEngine()
     let initialDevice: Device
-
+    
     let mic: AudioEngine.InputNode
     let tappableNodeA: Fader
     let tappableNodeB: Fader
     let tappableNodeC: Fader
     let silence: Fader
-
+    
     var tracker: PitchTap!
-
+    
     let noteFrequencies = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87]
     let noteNamesWithSharps = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
     let noteNamesWithFlats = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
-
+    
     init() {
         guard let input = engine.input else { fatalError() }
-
+        
         guard let device = engine.inputDevice else { fatalError() }
-
+        
         initialDevice = device
-
+        
         mic = input
         tappableNodeA = Fader(mic)
         tappableNodeB = Fader(tappableNodeA)
         tappableNodeC = Fader(tappableNodeB)
         silence = Fader(tappableNodeC, gain: 0)
         engine.output = silence
-
+        
         tracker = PitchTap(mic) { pitch, amp in
             DispatchQueue.main.async {
                 self.update(pitch[0], amp[0])
@@ -60,14 +60,14 @@ class TunerConductor: ObservableObject, HasAudioEngine {
         }
         tracker.start()
     }
-
+    
     func update(_ pitch: AUValue, _ amp: AUValue) {
         // Reduces sensitivity to background noise to prevent random / fluctuating data.
         guard amp > 0.1 else { return }
-
+        
         data.pitch = pitch
         data.amplitude = amp
-
+        
         var frequency = pitch
         while frequency > Float(noteFrequencies[noteFrequencies.count - 1]) {
             frequency /= 2.0
@@ -75,10 +75,10 @@ class TunerConductor: ObservableObject, HasAudioEngine {
         while frequency < Float(noteFrequencies[0]) {
             frequency *= 2.0
         }
-
+        
         var minDistance: Float = 10000.0
         var index = 0
-
+        
         for possibleIndex in 0 ..< noteFrequencies.count {
             let distance = fabsf(Float(noteFrequencies[possibleIndex]) - frequency)
             if distance < minDistance {
@@ -94,6 +94,7 @@ class TunerConductor: ObservableObject, HasAudioEngine {
 
 
 struct ContentView: View {
+    @Environment(\.scenePhase) private var scenePhase
     @StateObject var conductor = TunerConductor()
     
     var body: some View {
@@ -103,25 +104,25 @@ struct ContentView: View {
                 Spacer()
                 Text("\(conductor.data.pitch, specifier: "%0.1f")")
             }.padding()
-
+            
             HStack {
                 Text("Amplitude")
                 Spacer()
                 Text("\(conductor.data.amplitude, specifier: "%0.1f")")
             }.padding()
-
+            
             HStack {
                 Text("Note Name")
                 Spacer()
                 Text("\(conductor.data.noteNameWithSharps) / \(conductor.data.noteNameWithFlats)")
             }.padding()
-
+            
             InputDevicePicker(device: conductor.initialDevice)
-
+            
             NodeRollingView(conductor.tappableNodeA).clipped()
-
+            
             NodeOutputView(conductor.tappableNodeB).clipped()
-
+            
             NodeFFTView(conductor.tappableNodeC).clipped()
         }
         .navigationTitle("Tuner")
@@ -131,12 +132,21 @@ struct ContentView: View {
         .onDisappear {
             conductor.stop()
         }
+        .onChange(of: scenePhase) { oldPhase, newPhase in
+            if newPhase == .active {
+                if !self.conductor.engine.avEngine.isRunning {
+                    self.conductor.start()
+                }
+            } else if newPhase == .background {
+                conductor.stop()
+            }
+        }
     }
 }
 
 struct InputDevicePicker: View {
     @State var device: Device
-
+    
     var body: some View {
         Picker("Input: \(device.deviceID)", selection: $device) {
             ForEach(getDevices(), id: \.self) {
@@ -148,11 +158,11 @@ struct InputDevicePicker: View {
             setInputDevice(to: device)
         }
     }
-
+    
     func getDevices() -> [Device] {
         AudioEngine.inputDevices.compactMap { $0 }
     }
-
+    
     func setInputDevice(to device: Device) {
         do {
             try AudioEngine.setInputDevice(device)
